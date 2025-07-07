ae5528d40fe351c84af21f1f61189ad9085cf685
Nagarjuja Boddupalli <144195565+mrnagarjuna@users.noreply.github.com>
2025-07-07 12:38:29 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["3.109.124.43", "localhost", "127.0.0.1", "3.110.178.41"]
+ALLOWED_HOSTS = ["3.109.124.43", "localhost", "127.0.0.1", "13.204.75.82"]
 
 
 
